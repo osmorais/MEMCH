814910fe5d25973ddd4438c9590278d6e57b4f93
osmorais <osmorais5@gmail.com>
2020-04-09 00:59:06 -0300

    Correção da proc de inserção de registro para verificar a possibilidade de consumo excedido ou de surto

--- coletor/RegistroDAO.py
+++ coletor/RegistroDAO.py
@@ -8,33 +8,32 @@ class RegistroDAO(IRegistroDAO):
         #insert = "INSERT INTO REGISTRO (VALOR, DATA, HIDROMETROFK) VALUES (%s,%s,$s)"
         insert = "INSERT INTO REGISTRO (VALOR, DATA, HIDROMETROFK) VALUES {}"
 
-        conn = ConnectionFactory();
+        conn = ConnectionFactory()
         try:
             connection = conn.getConnection()
             cursor = connection.cursor()
 
             print("REGISTRO DAO")
-            print("valor : " , round(hidrometro.registro.valor),3)
+            print("valor : " , round(hidrometro.registro.valor,3))
             print("data: " + str(hidrometro.registro.data))
             print("hidrometro: " + str(hidrometro.id))
 
-            record_to_insert = (round(hidrometro.registro.valor,3),
-                                    str(hidrometro.registro.data),
-                                    hidrometro.id)
+            # record_to_insert = (round(hidrometro.registro.valor,3),
+            #                         str(hidrometro.registro.data),
+            #                         hidrometro.id)
+            #cursor.execute(insert.format(record_to_insert))
 
-            #print(insert.format(record_to_insert))
-            cursor.execute(insert.format(record_to_insert))
-
+            cursor.callproc('inserirRegistro',[
+                round(hidrometro.registro.valor,3),
+                str(hidrometro.registro.data),
+                hidrometro.id])
 
             connection.commit()
-
             #count = cursor.rowcount
             #return (count, "Record inserted successfully into mobile table")
-
         except (Exception, psycopg2.Error) as error:
             if (connection):
-                print("SQL Erro:", error)
-
+                print("SQL Error:", error)
         finally:
             # closing database connection.
             if (connection):
