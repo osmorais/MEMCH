80544ae0c3a53be028afd79962dd945ea931f629
Osmar Junior <osmorais5@gmail.com>
2020-08-16 19:39:52 -0400

    [FEAT] Metodo GET para recuperar o hidrometro

--- servidor/src/java/recurso/RecHidrometro.java
+++ servidor/src/java/recurso/RecHidrometro.java
@@ -17,6 +17,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 import modelo.Hidrometro;
 import servico.SrvcHidrometro;
@@ -106,6 +108,20 @@ public class RecHidrometro {
         String retorno = objgson.toJson(hidrometro);
 
         return retorno;
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("consultar/json/{id}")
+    public Response consultarJson(@PathParam("id") Integer id) {
+        
+        Hidrometro hidrometro = new Hidrometro();
+        hidrometro.setId(id);
+        SrvcHidrometro.consultar(hidrometro);
+        
+        String retorno = objgson.toJson(hidrometro);
+
+        return Response.status(200).entity(retorno).header("Access-Control-Allow-Origin", "*").build();
     }
     /**
      * PUT method for updating or creating an instance of RecHidrometro
